2a75db238527c4edcab73ca35254be900894965a
George Roubie <george.roubie@gmail.com>
2016-06-08 15:16:44 +0300

    upload theme

---
+++ ResponsiveThemeForDevelopers/Gruntfile.js
@@ -0,0 +1,45 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON ('package.json'),
+        less:{
+            development: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    'Styles/Site.css': 'Styles/Site.less'
+                }
+            }
+        },
+        concat: {
+            generated: {
+                files: [
+                    {
+                        dest: 'Scripts/Site.js',
+                        src: [
+                            'Scripts/jquery.js',
+                            'Scripts/jquery.cookiebar.js',
+                            'Scripts/bootstrap.js',
+                            'Scripts/Lib.js',
+                            'Scripts/toastr.js',
+                            'Scripts/Custom.js'
+                        ]
+                    }
+                ]
+            }
+        },
+        uglify: {
+            build: {
+                src: 'Scripts/Site.js',
+                dest: 'Scripts/Site.js'
+            }
+        }
+    });
+    // Load the plugin that provides the task
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['less', 'concat', 'uglify']);
+};
---
+++ ResponsiveThemeForDevelopers/Scripts/Custom.js
@@ -0,0 +1,54 @@
+﻿(function () {
+
+    //toast options
+    toastr.options = {
+        'showDuration': '1000',
+        'hideDuration': '1000',
+        'timeOut': '8000',
+        'progressBar': true,
+        'positionClass': 'toast-bottom-right'
+    };
+
+    //Disable Submit Button on Submit
+    $('form').submit(function () {
+        if ($(this).valid()) {
+            $(this).find('button[type="submit"]').prop('disabled', true)
+                                                 .attr('style', 'background:none!important;border:none!important')
+                                                 .html('<i class="fa fa-spinner fa-spin"></i>');
+        }
+    });
+
+    //Two level dropdown menu
+    $('ul.dropdown-menu > li.dropdown-submenu > a').on('click', function (event) {
+        // Avoid following the href location when clicking
+        event.preventDefault();
+        // Avoid having the menu to close when clicking
+        event.stopPropagation();
+
+        //opening and closing the submenu
+        if ($(this).parent().hasClass('open')) {
+            $(this).parent().removeClass('open');
+        } else {
+            $(this).parent().addClass('open');
+        }
+    });
+
+    //Toastr Messages
+    var messageElement;
+    $('.zone-messages .message').each(function () {
+        messageElement = $(this);
+        if (messageElement.hasClass('message-Information')) {
+            toastr.success(messageElement.html());
+        } else if (messageElement.hasClass('message-Warning')) {
+            toastr.warning(messageElement.html());
+        } else if (messageElement.hasClass('message-Error')) {
+            toastr.error(messageElement.html());
+        }
+    });
+    //Last Script
+    $.when($('#loader').fadeOut('slow')).done(function () {
+        $('#layout-wrapper').css('opacity', '1');
+        $.cookieBar();
+    });
+
+}());
