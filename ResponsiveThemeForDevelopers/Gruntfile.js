76f27e5921124327481788d3435c32efd55b4ea6
George Roubie <george.roubie@gmail.com>
2016-07-11 10:17:59 +0300

    Update the gruntfile
    Add watch task for faster development, and css prefixer for browser compatibily issues

--- ResponsiveThemeForDevelopers/Gruntfile.js
+++ ResponsiveThemeForDevelopers/Gruntfile.js
@@ -1,13 +1,31 @@
-﻿module.exports = function (grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON ('package.json'),
         less:{
             development: {
                 options: {
-                    compress: true
+                    compress: false
                 },
                 files: {
                     'Styles/Site.css': 'Styles/Site.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    'Styles/site.css': 'Styles/site.less'
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['Chrome 50', 'Firefox 12', 'Explorer 8', 'Explorer 9', 'Explorer 10', 'Explorer 11', 'Edge 12', 'iOS 6', 'Opera 35', 'Safari 5', 'ExplorerMobile 11']
+            },
+            dist: {
+                files: {
+                    'Styles/Site.css': 'Styles/Site.css'
                 }
             }
         },
@@ -33,13 +51,32 @@
                 src: 'Scripts/Site.js',
                 dest: 'Scripts/Site.js'
             }
+        },
+        watch: {
+            css: {
+                files: ['Styles/*.less'],
+                tasks: ['less:development', 'autoprefixer'],
+                options: {
+                    livereload: true
+                }
+            },
+            scripts: {
+                files: ['Scripts/Custom.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    livereload: true
+                }
+            }
         }
     });
+
     // Load the plugin that provides the task
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Default task(s).
-    grunt.registerTask('default', ['less', 'concat', 'uglify']);
+    // Tasks
+    grunt.registerTask('publish', ['less:production', 'autoprefixer', 'concat', 'uglify']);
 };
