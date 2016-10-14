ff8238349068755032a76e028be8f1095f539374
George Roubie <george.roubie@gmail.com>
2016-10-14 10:29:46 +0300

    add image minification task

---
+++ tasks/task.minify-img.js
@@ -0,0 +1,13 @@
+﻿var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    printSpaceSavings = require('gulp-print-spacesavings');
+
+gulp.task('minify-img', function () {
+    return gulp.src('./img/src/*')
+            .pipe(printSpaceSavings.init())
+            .pipe(imagemin({
+                progressive: true
+            }))
+            .pipe(printSpaceSavings.print())
+            .pipe(gulp.dest('./img/dist/'));
+});
