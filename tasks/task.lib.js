e3400aa98c8aae0d8ecd2ec597ca881da9be6b22
George Roubie <george.roubie@gmail.com>
2017-08-18 10:17:34 +0300

    remove lib folder

--- tasks/task.lib.js
+++ tasks/task.lib.js
@@ -25,7 +25,8 @@ var libPaths = {
         js: {
             bootstrap: `${basePaths.src.nodeModules}/bootstrap-sass/assets/javascripts/bootstrap.js`,
             toastr: `${basePaths.src.nodeModules}/toastr/toastr.js`,
-            jquery: `${basePaths.src.nodeModules}/jquery/dist/jquery.js`
+            jquery: `${basePaths.src.nodeModules}/jquery/dist/jquery.js`,
+            validation: `${basePaths.src.nodeModules}/jquery-validation/dist/jquery.validate.js`
         }
     }
 };
