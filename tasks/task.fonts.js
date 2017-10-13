d19634102ab818c58ed8106e638253a5196db22f
g.roubie <george.roubie@gmail.com>
2017-10-13 11:15:43 +0300

    get css from node_modules with sass, copy font files with gulp

---
+++ tasks/task.fonts.js
@@ -0,0 +1,26 @@
+﻿var gulp = require('gulp');
+
+var basePaths = {
+    src: {
+        nodeModules: './node_modules'
+    },
+    dest: {
+        rootFonts: './fonts'
+    }
+};
+
+var libPaths = {
+    src: {
+        fonts: {
+            bootstrap: `${basePaths.src.nodeModules}/bootstrap-sass/assets/fonts/bootstrap/*.*`,
+            fontawesome: `${basePaths.src.nodeModules}/font-awesome/fonts/*.*`
+        }
+    }
+};
+
+gulp.task('fonts', function () {
+    // fonts
+    for (var item in libPaths.src.fonts) {
+        gulp.src(libPaths.src.fonts[item]).pipe(gulp.dest(basePaths.dest.rootFonts))
+    }
+});
